82df8e753b6dde1cc9a5118b296165fa2d5588a9
seungjae <dhtmdwo73@naver.com>
2025-03-18 12:22:04 +0900

    good2

--- src/main/java/com/example/jenkins/Controller.java
+++ src/main/java/com/example/jenkins/Controller.java
@@ -1,11 +1,13 @@
 package com.example.jenkins;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
 public class Controller {
+    @GetMapping("/ex")
     public String ex(){
         return "ex01";
     }
