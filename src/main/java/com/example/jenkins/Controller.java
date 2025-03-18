9bd8e3b191b6876f53f348d7a546f45fe828c634
seungjae <dhtmdwo73@naver.com>
2025-03-18 11:32:00 +0900

    test

--- src/main/java/com/example/jenkins/Controller.java
+++ src/main/java/com/example/jenkins/Controller.java
@@ -1,4 +1,9 @@
 package com.example.jenkins;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class Controller {
 }
